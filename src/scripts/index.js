b3197032c24cba450b5c8242f7e99d3d1a8522d5
Albert Augusto <augustothelema@gmail.com>
2024-07-20 18:00:27 -0300

    Tarefa 4 concluída

---
+++ src/scripts/index.js
@@ -0,0 +1,30 @@
+const buttonHeader = document.querySelector(".header__button");
+const pageModal = document.querySelector(".dialog__container");
+
+buttonHeader.addEventListener("click", () => {
+    pageModal.showModal();
+
+    closeModal();
+});
+
+function closeModal(){
+    const buttonCloseModal = document.querySelector(".dialog__form__div__button");
+    const pageModal = document.querySelector(".dialog__container");
+
+    buttonCloseModal.addEventListener("click", () =>{
+        pageModal.close();
+    });
+}
+
+const buttonSection3 = document.querySelector(".section3__div1__button");
+
+buttonSection3.addEventListener("click", () => {
+    pageModal.showModal();
+
+    closeModal();
+});
+
+const formModal = document.querySelector(".dialog__form");
+formModal.addEventListener("submit",() =>{
+    alert("Cadastro Realizado com Sucesso")
+})
